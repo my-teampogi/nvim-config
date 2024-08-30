e7a7427c5b3f8c7ecf66799b6cb3de2cd7077541
Patrick Marino <pmmarino@multisyscorp.com>
2024-08-30 10:57:28 +0800

    first commit

---
+++ after/plugin/neosolarized.lua
@@ -0,0 +1,18 @@
+local status, n = pcall(require, 'neosolarized')
+if (not status) then return end
+
+n.setup({
+  commit_italics = true })
+
+local colorbuddy = require('colorbuddy.init')
+local Color = colorbuddy.Color
+local colors = colorbuddy.colors
+local Group = colorbuddy.Group
+local groups = colorbuddy.groups
+local styles = colorbuddy.styles
+
+Color.new('black', '#000000')
+Group.new('CursorLine', colors.none, colors.base03, styles.NONE, colors.base1)
+Group.new('CursorLineNr', colors.yellow, colors.black, styles.NONE, colors.base1)
+Group.new('Visual', colors.none, colors.base03, styles.reverse)
+
